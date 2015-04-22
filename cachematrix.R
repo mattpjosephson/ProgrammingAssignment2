74bd72c28e4fa7b64bed047fd05134e7bfb3c8f0
Matthew Josephson <Mattp.josephson@gmail.com>
2015-04-22 10:16:09 -0500

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## function will create a matrix 'x' that will later be inverted
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL   ## i will eventually be inverse of matrix, but at this point is not calculated yet, so NULL
+        set <- function(y) {    ## set is a function that changes the matrix stored originally to be y
+                x <<- y         ## sets matrix x (<<- means from makeCacheMatrix, not within set function) to be  y (the input)
+                i <<- NULL      ## sets i (<<- means from makeCache Matrix, not within set function) to be NULL, 
+        }                      
+        get <- function() x     ## get is a function that returns "x"
+        setinverse <- function(solve) i <<- solve       ##set i (from original makeCacheMatrix) to 'solve'
+        getinverse <- function() i ## returns the value i (lexical scoping finds i by searching all enviornments )
+        list(set = set, 
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve will calculate, or return a previously cached value for the inverse of matrix 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()             ## searches getinverse function for cached value of inverse of 'x'
+                if(!is.null(i)) {
+                        message("getting cached data")
+                        return(i)       ## returns cached value for inverse of 'x' if present
+        }
+        data <- x$get()                 ## if no cached value for inverse of 'x', then it is calculated, cached, and printed.
+        i <- solve(data, ...)
+        x$setinverse(i)
+        print(i)
 }
